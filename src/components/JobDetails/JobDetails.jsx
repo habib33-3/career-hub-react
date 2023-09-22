4bcd8491e55df2aa4e76d8227726835b9bdb62ab
Habibur Rahman <habib.swi16@gmail.com>
2023-09-22 15:14:47 +0600

    load job application in localstorage

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { saveJobApplication } from "../../util/localStorage";
 
 const JobDetails = () => {
   const jobs = useLoaderData();
@@ -11,6 +12,7 @@ const JobDetails = () => {
   const { job_title } = job;
 
   const handleApplyJob = () => {
+    saveJobApplication(id);
     toast("You have applied successfully");
   };
 
